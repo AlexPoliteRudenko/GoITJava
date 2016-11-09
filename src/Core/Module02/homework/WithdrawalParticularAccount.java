8f7c07963d90cd5578f6b39d08c703e92b2557ff
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-11-09 13:58:10 +0200

    Fix 2.3

--- src/Core/Module02/homework/WithdrawalParticularAccount.java
+++ src/Core/Module02/homework/WithdrawalParticularAccount.java
@@ -27,29 +27,28 @@ public class WithdrawalParticularAccount {
     public static void main(String[] args) {
         int[] balances = {1200, 250, 2000, 500, 3200};
         String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-//        String ownerName = "Ann";
-//        double withdrawal = 100;
-        String ownerName = "Oww";
-        double withdrawal = 490;
-        int index = 0;
-        for (String item : ownerNames) {
-            if (item.equals(ownerName)) {
+        String ownerName = "Ann";
+        double withdrawal = 100;
+//        String ownerName = "Oww";
+//        double withdrawal = 490;
+        for (int i = 0; i < ownerNames.length; i++) {
+            if (ownerNames[i] == ownerName) {
                 System.out.print(ownerName + " ");
-                withdrawal(withdrawal, balances, index);
+                withdrawal(withdrawal, balances[i]);
+
             }
-            index++;
         }
     }
 
-    private static void withdrawal(double withdrawal, int[] balances, int index) {
+    private static void withdrawal(double withdrawal, int balance) {
         double commission = 5;
         double withdrawCommission = withdrawal * commission / 100;
         double withdraw = withdrawal + withdrawCommission;
-        if (withdraw > balances[index]) {
+        if (withdraw > balance) {
             System.out.println("NO");
         } else {
-            balances[index] -= withdraw;
-            System.out.println(withdrawal + " " + balances[index]);
+            balance -= withdraw;
+            System.out.println(withdrawal + " " + balance);
         }
     }
 }
--- src/Core/Module03/Homework/Bird.java
+++ src/Core/Module03/Homework/Bird.java
@@ -1,5 +1,7 @@
 package Core.Module03.Homework;
 
 public class Bird {
+    public static void main(String[] args) {
 
+    }
 }
