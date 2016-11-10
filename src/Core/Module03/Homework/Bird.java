f4d06d41d4d5f2702915479a3de4037482654b98
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-11-10 11:33:17 +0200

    HW 3.1

--- src/Core/Module03/Homework/Bird.java
+++ src/Core/Module03/Homework/Bird.java
@@ -4,4 +4,12 @@ public class Bird {
     public static void main(String[] args) {
 
     }
+
+    public void sing() {
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+
 }
