74803342ef292554451f6047748e89bd23ff358e
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-11-10 11:33:40 +0200

    HW 3.1

---
+++ src/Core/Module03/Homework/Main.java
@@ -0,0 +1,11 @@
+package Core.Module03.Homework;
+
+public class Main {
+    public static void main(String[] args) {
+//        Task 3.1
+        Bird bird1 = new Bird();
+        bird1.sing();
+
+
+    }
+}
