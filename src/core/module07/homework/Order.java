e16e078e48724a129914b1e7044ecff7ddfe58aa
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-12-07 12:30:50 +0200

    HW module 7

---
+++ src/core/module07/homework/Order.java
@@ -0,0 +1,14 @@
+package core.module07.homework;
+
+
+import core.module04.homework.Currency;
+
+public class Order {
+
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+    private User user;
+}
